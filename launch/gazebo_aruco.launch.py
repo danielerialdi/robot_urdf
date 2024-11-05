304bc4039f4b353e6716ff42f35acfd48e77cc2f
danielerialdi <daniele.rialdi@gmail.com>
2024-11-05 14:10:32 +0000

    And yet it moves

--- launch/gazebo_aruco.launch.py
+++ launch/gazebo_aruco.launch.py
@@ -13,38 +13,52 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    test_robot_description_share = FindPackageShare(package='robot_urdf').find('robot_urdf')
-    default_model_path = os.path.join(test_robot_description_share, 'urdf/robot4.xacro')
+    test_robot_description_share = FindPackageShare(
+        package='robot_urdf').find('robot_urdf')
+    default_model_path = os.path.join(
+        test_robot_description_share, 'urdf/robot4.xacro')
     # default_world_path = os.path.join(test_robot_description_share, 'worlds/aruco_test.world')
-    default_world_path = os.path.join(test_robot_description_share, 'worlds/aruco_5.world')
-    rviz_config_path = os.path.join(test_robot_description_share, 'config/rviz.rviz')
+    default_world_path = os.path.join(
+        test_robot_description_share, 'worlds/aruco_5.world')
+    rviz_config_path = os.path.join(
+        test_robot_description_share, 'config/rviz.rviz')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(
+            ['xacro ', LaunchConfiguration('model')])}]
     )
-    
+
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher'
     )
-   
 
+    
+    
     # GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-entity', 'my_test_robot', '-topic', '/robot_description', '-Y', '3.14'],
+                        arguments=['-entity', 'my_test_robot',
+                                   '-topic', '/robot_description', '-Y', '3.14'],
                         output='screen')
 
+    camera01_controller = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        output="screen",
+        arguments=["joint_camera_controller"],
+    )
     return LaunchDescription([
         DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                    description='Absolute path to robot urdf file'),
+                              description='Absolute path to robot urdf file'),
         robot_state_publisher_node,
         joint_state_publisher_node,
         spawn_entity,
         ExecuteProcess(
-            cmd=['gazebo', '--verbose', default_world_path, '-s', 'libgazebo_ros_factory.so'],
+            cmd=['gazebo', '--verbose', default_world_path,
+                 '-s', 'libgazebo_ros_factory.so'],
             output='screen'),
         ExecuteProcess(
             cmd=['rviz2', '-d', rviz_config_path],
